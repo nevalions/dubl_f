7b847cc89c0d13f097c347a745eba02ed7d3b9e6
linroot <linroot@bk.ru>
2023-02-17 22:31:13 +0300

    refactoring

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-prefix = f'http://'
-server = f'127.0.0.1'
-sep = f':'
-port = f'8088'
+prefix = 'http://'
+server = '127.0.0.1'
+sep = ':'
+port = '8088'
 
 vmix_inputs_titles = ['teams', 'tablo', 'stats', 'sostav_a', 'sostav_b', 'refs']
 
