694063efc5319e957345854daa157aa14dd41300
linroot <linroot@bk.ru>
2023-02-17 21:56:12 +0300

    refactoring

--- get_guids.py
+++ get_guids.py
@@ -1,37 +1,64 @@
 import os.path
 import sys
+import typing
 
 import requests
+import json
+
 from bs4 import BeautifulSoup
 
 from config import URL
 
 url = f'{URL}/API/'
+foldername = 'data'
+xml_filename = 'guids.xml'
 
-try:
-    if not os.path.exists('data'):
-        os.mkdir('data')
-except Exception as ex:
-    print(ex)
-    print('Error creating dir')
-    sys.exit()
 
-try:
-    r = requests.get(url)
-    with open('data/guids.xml', 'w') as file_xml:
-        file_xml.write(r.text)
-except Exception as ex:
-    print(ex)
-    print('Vmix not running, or can not connect')
-    sys.exit()
+def main():
+    get_vmix_local_xml(url, foldername, xml_filename)
+    pars_vmix_xml(foldername, xml_filename)
 
-with open('data/guids.xml') as file:
-    src = file.read()
 
-soup = BeautifulSoup(src, 'lxml')
-teams_guid_pars = soup.find('input', title='teams').attrs['key']
-tablo_guid_pars = soup.find('input', title='tablo').attrs['key']
-stats_guid_pars = soup.find('input', title='stats').attrs['key']
-sostav_a_guid_pars = soup.find('input', title='sostav_a').attrs['key']
-sostav_b_guid_pars = soup.find('input', title='sostav_b').attrs['key']
-refs_guid_pars = soup.find('input', title='refs').attrs['key']
+def get_vmix_local_xml(u: str, s_foldername: str, s_filename: str) -> typing:
+    try:
+        if not os.path.exists(s_foldername):
+            os.mkdir(s_foldername)
+    except Exception as ex:
+        print(ex)
+        print('Error creating dir')
+        sys.exit()
+
+    try:
+        r = requests.get(u)
+        with open(f'{s_foldername}/{s_filename}', 'w') as file_xml:
+            file_xml.write(r.text)
+    except Exception as ex:
+        print(ex)
+        print('Vmix not running, or can not connect')
+        sys.exit()
+
+
+def pars_vmix_xml(o_foldername: str, o_filename: str) -> typing:
+    try:
+        with open(f'{o_foldername}/{o_filename}') as file:
+            src = file.read()
+    except Exception as ex:
+        print(ex)
+        print('Error opening JSON file')
+        sys.exit()
+
+    vmix_inputs_titles = ['teams', 'tablo', 'stats', 'sostav_a', 'sostav_b', 'refs']
+    parsed_match_dict = {}
+
+    soup = BeautifulSoup(src, 'lxml')
+
+    for title in vmix_inputs_titles:
+        guid = soup.find('input', title=title).attrs['key']
+        parsed_match_dict[title] = guid
+
+    with open(f'{o_foldername}/parsed_guids.json', 'w') as output_json:
+        output_json.write(json.dumps(parsed_match_dict, indent=4))
+
+
+if __name__ == '__main__':
+    main()
--- inject_urls.py
+++ inject_urls.py
@@ -1,38 +1,57 @@
+import json
 import urllib.request
-
-from get_guids import teams_guid_pars, tablo_guid_pars, stats_guid_pars, sostav_a_guid_pars, sostav_b_guid_pars, refs_guid_pars
 
 from config import URL
 
-vmix_browser = f'{URL}/api/?Function=BrowserNavigate&Value='
+folder = 'data'
+json_file = 'parsed_guids.json'
 
-basket_web = 'http%3A%2F%2Fig.russiabasket.ru%2F'
 
-competition = '%26compId%3D39879%26db%3Dorg%26tab%3D0%26tv%3D1%26color%3D4%26league%3D4%26'
-game_id = '169247'
+def main():
+    game_id = '169247'
 
-teams_guid = f'&Input={teams_guid_pars}'
-teams = f'preview%2F%3FgameId%3D{game_id}{competition}region%3D1%26short%3D0%26teamA%3D%26teamB%3D%26leaguelogo%3D0{teams_guid}'
-teams_api = urllib.request.urlopen(vmix_browser + basket_web + teams)
+    guids = get_parsed_guid_from_json(folder, json_file)
+    inject_urls_to_vmix(game_id, guids)
 
-tablo_guid = f'&Input={tablo_guid_pars}'
-tablo_teams = f'online%2F%3Fid%3D{game_id}{competition}logo%3D3%26foul%3D1%26white%3D1%26blank%3D6%26short%3D1%26teamA%3D%26teamB%3D{tablo_guid}'
-tablo_api = urllib.request.urlopen(vmix_browser + basket_web + tablo_teams)
 
-stats_guid = f'&Input={stats_guid_pars}'
-tablo_stats = f'online-stat%2F%3Fid%3D{game_id}{competition}logo%3D3%26foul%3D1%26blank%3D6%26short%3D0%26teamA%3D%26teamB%3D{stats_guid}'
-stats_api = urllib.request.urlopen(vmix_browser + basket_web + tablo_stats)
+def get_parsed_guid_from_json(foldername: str, json_filename: str) -> dict:
+    with open(f'{foldername}/{json_filename}', 'r') as file:
+        guids = json.load(file)
+    return guids
 
-rosters = 'online-roster%2F%3FgameId%3D'
-roster_url_part_one = f'{competition}teamNumber%3D'
-roster_url_part_two = '%26teamA%3D%26teamB%3D'
-sostav_a_guid = f'&Input={sostav_a_guid_pars}'
-sostav_a = f'{rosters}{game_id}{roster_url_part_one}1{roster_url_part_two}{sostav_a_guid}'
-sostav_a_api = urllib.request.urlopen(vmix_browser + basket_web + sostav_a)
-sostav_b_guid = f'&Input={sostav_b_guid_pars}'
-sostav_b = f'{rosters}{game_id}{roster_url_part_one}2{roster_url_part_two}{sostav_b_guid}'
-sostav_b_api = urllib.request.urlopen(vmix_browser + basket_web + sostav_b)
 
-refs_guid = f'&Input={refs_guid_pars}'
-refs = f'online-ref%2F%3FgameId%3D{game_id}{competition}{refs_guid}'
-refs_api = urllib.request.urlopen(vmix_browser + basket_web + refs)
+def inject_urls_to_vmix(g_id: str, parsed_guids: dict):
+
+    vmix_browser = f'{URL}/api/?Function=BrowserNavigate&Value='
+    basket_web = 'http%3A%2F%2Fig.russiabasket.ru%2F'
+    competition = '%26compId%3D39879%26db%3Dorg%26tab%3D0%26tv%3D1%26color%3D4%26league%3D4%26'
+
+    teams_guid = f'&Input={parsed_guids["teams"]}'
+    teams = f'preview%2F%3FgameId%3D{g_id}{competition}region%3D1%26short%3D0%26teamA%3D%26teamB%3D%26leaguelogo%3D0{teams_guid}'
+    urllib.request.urlopen(vmix_browser + basket_web + teams)
+
+    tablo_guid = f'&Input={parsed_guids["tablo"]}'
+    tablo_teams = f'online%2F%3Fid%3D{g_id}{competition}logo%3D3%26foul%3D1%26white%3D1%26blank%3D6%26short%3D1%26teamA%3D%26teamB%3D{tablo_guid}'
+    urllib.request.urlopen(vmix_browser + basket_web + tablo_teams)
+
+    stats_guid = f'&Input={parsed_guids["stats"]}'
+    tablo_stats = f'online-stat%2F%3Fid%3D{g_id}{competition}logo%3D3%26foul%3D1%26blank%3D6%26short%3D0%26teamA%3D%26teamB%3D{stats_guid}'
+    urllib.request.urlopen(vmix_browser + basket_web + tablo_stats)
+
+    rosters = 'online-roster%2F%3FgameId%3D'
+    roster_url_part_one = f'{competition}teamNumber%3D'
+    roster_url_part_two = '%26teamA%3D%26teamB%3D'
+    sostav_a_guid = f'&Input={parsed_guids["sostav_a"]}'
+    sostav_a = f'{rosters}{g_id}{roster_url_part_one}1{roster_url_part_two}{sostav_a_guid}'
+    urllib.request.urlopen(vmix_browser + basket_web + sostav_a)
+    sostav_b_guid = f'&Input={parsed_guids["sostav_b"]}'
+    sostav_b = f'{rosters}{g_id}{roster_url_part_one}2{roster_url_part_two}{sostav_b_guid}'
+    urllib.request.urlopen(vmix_browser + basket_web + sostav_b)
+
+    refs_guid = f'&Input={parsed_guids["refs"]}'
+    refs = f'online-ref%2F%3FgameId%3D{g_id}{competition}{refs_guid}'
+    urllib.request.urlopen(vmix_browser + basket_web + refs)
+
+
+if __name__ == '__main__':
+    main()
